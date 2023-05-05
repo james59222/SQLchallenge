2f8cfd67f3b78ff0da76db4b2ef6b6057828b8a2
james59222 <james59222@gmail.com>
2023-05-04 20:44:39 -0500

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 let db = mysql.createConnection ({
     host:'localhost', 
     user:'root',
-    password:'Jimmyp33##',
+    password:'',
     port: 3306,
     database:'employee_db',
 })
